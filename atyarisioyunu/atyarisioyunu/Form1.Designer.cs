464d28786fdbb3d5f62322aeb1c2777206d99725
akifdlhan <112507694+akifdlhan@users.noreply.github.com>
2022-08-30 20:58:41 +0300

    Add files via upload

---
+++ atyarisioyunu/atyarisioyunu/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace atyarisioyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_basla = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.lbl_durum = new System.Windows.Forms.Label();
+            this.lbl_sure = new System.Windows.Forms.Label();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl
+            // 
+            this.lbl.BackColor = System.Drawing.Color.White;
+            this.lbl.Location = new System.Drawing.Point(0, 122);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(1367, 23);
+            this.lbl.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 271);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1367, 23);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(0, 421);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1367, 23);
+            this.label2.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(0, -5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1367, 23);
+            this.label4.TabIndex = 4;
+            // 
+            // btn_basla
+            // 
+            this.btn_basla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_basla.Location = new System.Drawing.Point(12, 460);
+            this.btn_basla.Name = "btn_basla";
+            this.btn_basla.Size = new System.Drawing.Size(163, 93);
+            this.btn_basla.TabIndex = 6;
+            this.btn_basla.Text = "BAŞLA";
+            this.btn_basla.UseVisualStyleBackColor = false;
+            this.btn_basla.Click += new System.EventHandler(this.btn_basla_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 87);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Red;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(0, 2);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "DELİDUMRUL";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Red;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(0, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "RUS ATI";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Red;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(0, 278);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 16);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "GEDİFE";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 171);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(132, 87);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(3, 321);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(132, 87);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(141, 315);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(990, 93);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(141, 165);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(1011, 93);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 14;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(141, 26);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(1011, 93);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 15;
+            this.pictureBox6.TabStop = false;
+            // 
+            // lbl_durum
+            // 
+            this.lbl_durum.BackColor = System.Drawing.Color.Black;
+            this.lbl_durum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_durum.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_durum.Location = new System.Drawing.Point(195, 460);
+            this.lbl_durum.Name = "lbl_durum";
+            this.lbl_durum.Size = new System.Drawing.Size(953, 93);
+            this.lbl_durum.TabIndex = 16;
+            this.lbl_durum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_sure
+            // 
+            this.lbl_sure.BackColor = System.Drawing.Color.Black;
+            this.lbl_sure.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_sure.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_sure.Location = new System.Drawing.Point(1154, 460);
+            this.lbl_sure.Name = "lbl_sure";
+            this.lbl_sure.Size = new System.Drawing.Size(170, 93);
+            this.lbl_sure.TabIndex = 17;
+            this.lbl_sure.Text = "0";
+            this.lbl_sure.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(1244, 2);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(123, 442);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 18;
+            this.pictureBox7.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(1366, 565);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.lbl_sure);
+            this.Controls.Add(this.lbl_durum);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_basla);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_basla;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label lbl_durum;
+        private System.Windows.Forms.Label lbl_sure;
+        private System.Windows.Forms.PictureBox pictureBox7;
+    }
+}
+
---
+++ atyarisioyunu/atyarisioyunu/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace atyarisioyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int delidumrulsolauzaklik, rusatisolauzaklik, gedifesolauzaklik;
+        Random ras =new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int delidumrulgenislik = pictureBox1.Width;
+            int rusatigenislik = pictureBox2.Width;
+            int gedifegenislik = pictureBox3.Width;
+            int fnish = pictureBox7.Left;
+            int sure = Convert.ToInt32(lbl_sure.Text);
+            sure++;
+            lbl_sure.Text = sure.ToString();
+            pictureBox1.Left += ras.Next(5,16);
+            pictureBox2.Left+=ras.Next(5,16);
+            pictureBox3.Left+=ras.Next(5,16);
+            
+            if (delidumrulgenislik + pictureBox1.Left >= fnish)
+            {
+                if(gedifegenislik + pictureBox2.Left > rusatigenislik) { 
+                timer1.Enabled = false;
+                pictureBox6.Visible = true;
+                MessageBox.Show("delidumrul kazandııııııııı\n2.gedife\n3.rusatı");
+               }else
+                {
+                    timer1.Enabled = false;
+                    pictureBox6.Visible = true;
+                    MessageBox.Show("delidumrul kazandııııııııı\n2.rusatı\n3.gedife");
+                }
+            }
+            else if (rusatigenislik + pictureBox2.Left >= fnish)
+            {
+                if (delidumrulgenislik + pictureBox3.Left > gedifegenislik)
+                {
+                    timer1.Enabled = false;
+                    pictureBox5.Visible = true;
+                    MessageBox.Show("rus atı kazandıııııııııııı\n2.delidumrul\n3.gedife");
+                }
+                else
+                {
+                    timer1.Enabled = false;
+                    pictureBox5.Visible = true;
+                    MessageBox.Show("rus atı kazandıııııııııııı\n2.gedife\n3.delidumrul");                    
+                }
+               
+            }
+            else if(gedifegenislik + pictureBox3.Left >= fnish)
+            {
+                if (delidumrulgenislik + pictureBox1.Left > rusatigenislik + pictureBox2.Left)
+                {
+                   timer1.Enabled = false;
+                   pictureBox4.Visible = true  ;
+                   MessageBox.Show("gedife kazandıııııııııııııı\n2.delidumrul\n3.rusatı");
+                }
+                else
+                {
+                    timer1.Enabled = false;
+                    pictureBox4.Visible = true;
+                    MessageBox.Show("gedife kazandıııııııııııııı\n2.rusatı\n3.delidumrul");                    
+                } 
+            }
+            if(pictureBox1.Left>pictureBox2.Left  && pictureBox1.Left > pictureBox3.Left )
+            {
+                lbl_durum.Text = "delidumrul yarışı önde götürüyor";
+            }
+            if (pictureBox2.Left > pictureBox1.Left && pictureBox2.Left > pictureBox3.Left )
+            {
+                lbl_durum.Text = "rus atı harika bir şekilde önde ilerliyor";
+            }
+            if (pictureBox3.Left > pictureBox1.Left && pictureBox3.Left > pictureBox2.Left)
+            {
+                lbl_durum.Text = "gedife atak yaptı uçuyor emin adımlarla 1.liğe";
+            }   
+        }
+        private void btn_basla_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = false;
+            delidumrulsolauzaklik = pictureBox1.Left;
+            rusatisolauzaklik = pictureBox2.Left;
+            gedifesolauzaklik = pictureBox3.Left;
+        }
+    }
+}
